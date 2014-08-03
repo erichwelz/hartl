6ca0a72cad7e2a0f24905693f62ce8399529da77
Erich Welz <erichwelz@gmail.com>
2014-08-03 09:35:33 -0400

    Finish sign in, one test failing

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,32 +33,40 @@ describe "User pages" do
       end
     end
 
-      describe "after submission" do
-        before { click_button submit }
+    describe "after submission" do
+      before { click_button submit }
 
-        it { should have_title('Sign up')}
-        it { should have_content('error')}
+      it { should have_title('Sign up')}
+      it { should have_content('error')}
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
 
-        describe "with valid information" do
-          before do
-            fill_in "Name",         with: "Example User"
-            fill_in "Email",        with: "user@example.com"
-            fill_in "Password",     with: "foobar"
-            fill_in "Confirmation", with: "foobar"
-          end
 
-          describe "after saving the user" do
-            before { click_button submit }
-            let (:user) {User.find_by(email: 'user@example.com' )}
+    describe "after saving the user" do
+      before { click_button submit }
+      let (:user) {User.find_by(email: 'user@example.com' )}
 
-            it { should have_title(user.name) }
-            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-          end
+      it { should have_link('Sign out') }
+      it { should have_title(user.name) }
+      it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    end
+
+      describe "followed by signout" do
+        before { click_link 'Sign out' }
+        it { should have_link('Sign in') }
+      end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
     end
   end
 end
